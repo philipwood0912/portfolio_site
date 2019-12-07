dd2ffea320aa1431043fb71c61ef08b808dfaeef
Philip <philipwood0912@gmail.com>
2019-12-07 01:24:10 -0500

    heroku config

--- config.js
+++ config.js
@@ -1,9 +1,16 @@
-const config = {
+/*const config = {
     host: "localhost",
     port: 8889, // 8889 for mac 3306 for windows
     uname: "root",
     pword: "root", // leave blank for windows
     database: "db_portfolio"
-}
+} */
 
+const config = {
+    host: "cleardb.net",
+    port: 8889, // 8889 for mac 3306 for windows
+    uname: "ba3ea472d057a9",
+    pword: "r7cea784f", // leave blank for windows
+    database: "heroku_0f8edada9fcc808"
+}
 module.exports = config;
